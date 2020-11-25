68faec3ef82259846db9ab1f123185047ae1891d
JARNO MAES <JARNO.MAES@student.intecbrussel.be>
2020-11-25 14:21:18 +0100

    Add project files.

---
+++ ArraysOfObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[2];
+            myArray[0] = 5;
+            myArray[1] = 10;
+
+
+            Person[] peopleArray = new Person[2];
+            peopleArray[0] = new Person("Bob", 5);
+            peopleArray[1] = new Person("Test", 6);
+
+            for (int i = 0; i < peopleArray.Length; i++)
+            {
+                Console.WriteLine($"{peopleArray[i].Name}, {peopleArray[i].Age}");
+            }
+        }
+    }
+}
